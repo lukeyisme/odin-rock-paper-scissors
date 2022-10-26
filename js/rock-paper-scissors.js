c855405d64be8b7af02be59776f5e446691cf750
Luke Curry <lukeyisme@gmail.com>
2022-10-25 21:00:49 -0400

    Refined the js functions, prints the winner and scores in the console

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,10 +1,27 @@
 let compNum;
 let compChoice;
 let userChoice;
+let compScore;
+let userScore;
 
 function game(){
-    for(let i = 0; i < 5; i++)
-    playRound()
+    userScore = 0;
+    compScore = 0;
+    
+    for(let i = 0; i < 5; i++){
+        playRound()
+    }
+
+    console.log(`Your score: ${userScore}`);
+    console.log(`Computer score: ${compScore}`);
+    
+    if(compScore > userScore){
+        console.log("The Computer is the winner! Better luck next time!")
+    }
+
+    else if(compScore < userScore){
+        console.log("You are the winner! Congratulations!")
+    }
 }
 
 function getComputerChoice(){
@@ -31,26 +48,32 @@ function playRound(){
 
     if(compChoice == 'rock' && userChoice.toLowerCase() == 'scissors'){
         console.log("Sorry, rock beats scissors!")
+        compScore++;
     }
 
     else if(compChoice == 'rock' && userChoice.toLowerCase() == 'paper'){
         console.log("Paper covers rock, you win!")
+        userScore++;
     }
 
     else if(compChoice == 'paper' && userChoice.toLowerCase() == 'rock'){
         console.log("Sorry, paper covers rock!")
+        compScore++;
     }
 
     else if(compChoice == 'paper' && userChoice.toLowerCase() == 'scissors'){
         console.log("Scissors cut paper, you win!")
+        userScore++;
     }
 
     else if(compChoice == 'scissors' && userChoice.toLowerCase() == 'paper'){
         console.log("Sorry, scissors cut paper!")
+        compScore++;
     }
 
     else if(compChoice == 'scissors' && userChoice.toLowerCase() == 'rock'){
         console.log("Rock beats scissors, you win!")
+        userScore++;
     }
 
     else if(compChoice == userChoice.toLowerCase()){
