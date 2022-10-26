c879534d9f3c36963f9517c71e2650fbaf75c819
Luke Curry <lukeyisme@gmail.com>
2022-10-25 20:21:55 -0400

    Added playRound function and section for user imput from the browser; game must still be called from the terminal.

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,20 +1,51 @@
 let compNum;
 let compChoice;
+let userChoice;
+
+userChoice = prompt("Choose rock, paper, or scissors:");
 
 function getComputerChoice(){
     compNum = Math.random();
 
     if(compNum <= 0.34){
-        compChoice = 'Rock';
+        compChoice = 'rock';
     }
 
     else if (compNum >= 0.67){
-        compChoice = 'Scissors'
+        compChoice = 'scissors'
     }
 
     else {
-        compChoice = 'Paper'
+        compChoice = 'paper'
     }
 
-    console.log(compChoice);
+    return compChoice;
+};
+
+function playRound(userChoice){
+    getComputerChoice();
+
+    if(compChoice == 'rock' && userChoice.toLowerCase() == 'scissors'){
+        console.log("Sorry, rock beats scissors!")
+    }
+
+    else if(compChoice == 'rock' && userChoice.toLowerCase() == 'paper'){
+        console.log("Paper covers rock, you win!")
+    }
+
+    else if(compChoice == 'paper' && userChoice.toLowerCase() == 'rock'){
+        console.log("Sorry, paper covers rock!")
+    }
+
+    else if(compChoice == 'paper' && userChoice.toLowerCase() == 'scissors'){
+        console.log("Scissors cut paper, you win!")
+    }
+
+    else if(compChoice == 'scissors' && userChoice.toLowerCase() == 'paper'){
+        console.log("Sorry, scissors cut paper!")
+    }
+
+    else if(compChoice == 'scissors' && userChoice.toLowerCase() == 'rock'){
+        console.log("Rock beats scissors, you win!")
+    }
 };
